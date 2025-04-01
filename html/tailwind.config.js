0215b9afe7e76d1ece34844ebe239c041268a43f
kiyeonkimm <paynjoy2@naver.com>
2025-04-01 10:18:58 +0900

    html first commit

---
+++ html/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        carrot: "#e67e22",
+        main: "#fd79a8",
+        skblue: "#81ecec",
+        danger: "#e3342f",
+      },
+    },
+  },
+};
